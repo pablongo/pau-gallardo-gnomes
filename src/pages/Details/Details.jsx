076ddc6a4750a3732aede0df9a14b8b7ed57147b
Pau Gallardo <paugallrdocordon@gmail.com>
2021-10-06 23:14:50 +0200

    Refactor Details page
    - Fix refresh bug

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 
+import { loadGnomes } from '../../redux/actions/actionsCreator';
+
 export default function Details() {
+  const dispatch = useDispatch();
+
   const { gnomeId } = useParams();
 
   const gnome = useSelector((store) => store.gnomes).find(({ id }) => id === +gnomeId);
+
+  useEffect(() => {
+    if (!gnome) {
+      dispatch(loadGnomes());
+    }
+  }, []);
 
   return (
     <>
