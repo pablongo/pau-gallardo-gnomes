f70e431b2886caa7771ca36b8796da17b0dbfb1f
Pau Gallardo <paugallrdocordon@gmail.com>
2021-10-06 15:02:10 +0200

    Install tooling
    - Add axios
    - Add redux-thunk

--- src/index.jsx
+++ src/index.jsx
@@ -5,9 +5,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode />,
   document.getElementById('root'),
 );
 
