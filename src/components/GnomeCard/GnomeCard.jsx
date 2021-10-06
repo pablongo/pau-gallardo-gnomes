ac9b7dceeef013d81ae3919db948865809e043a9
Pau Gallardo <paugallrdocordon@gmail.com>
2021-10-06 20:25:55 +0200

    Create details page

--- src/components/GnomeCard/GnomeCard.jsx
+++ src/components/GnomeCard/GnomeCard.jsx
@@ -1,15 +1,16 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './GnomeCard.scss';
 
 export default function GnomeCard({ gnome }) {
   return (
-    <>
+    <Link to={`/detail/${gnome.id}`}>
       <li className="gnome-container">
         <img className="gnome-container__avatar" src={gnome.thumbnail} alt={gnome.name} />
         <h3 className="gnome-container__title">{gnome?.name}</h3>
       </li>
-    </>
+    </Link>
   );
 }
--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router';
 
 export default function Details() {
+  const { gnomeId } = useParams();
+
+  const gnome = useSelector((store) => store.gnomes).find(({ id }) => id === +gnomeId);
+
   return (
-    <h1>Details Page</h1>
+    <>
+      <h1>Details Page</h1>
+      <h3>{gnome?.name}</h3>
+    </>
   );
 }
