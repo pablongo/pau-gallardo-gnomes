64420a104f7c302114354f83db3b7d3e43eca307
Pau Gallardo <paugallrdocordon@gmail.com>
2021-10-06 18:16:04 +0200

    Set up pagination reducer

--- src/redux/reducers/paginationReducer.js
+++ src/redux/reducers/paginationReducer.js
@@ -1,6 +1,9 @@
 import actionTypes from '../actions/actionTypes';
 
-export default function paginationReducer(pagination = {}, action) {
+export default function paginationReducer(
+  pagination = { currentPage: 1, gnomesPerPage: 20 },
+  action,
+) {
   let newPagination = pagination;
   switch (action.type) {
     case actionTypes.SET_PAGINATION:
