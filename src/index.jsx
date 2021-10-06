223c06fb33fe30696079597471096cc124e6c52a
Pau Gallardo <paugallrdocordon@gmail.com>
2021-10-06 20:12:34 +0200

    Create routes
    - Create NotFound page
    - Create Details page
    - Set up default routing

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter, Switch, Route, Redirect,
+} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import configureStore from './redux/store';
 
 import Dashboard from './pages/Dashboard/Dashboard';
+import Details from './pages/Details/Details';
+import NotFound from './pages/NotFound/NotFound';
 
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={configureStore()}>
-      <Dashboard />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={configureStore()}>
+        <Switch>
+          <Route path="/" exact component={Dashboard} />
+          <Redirect path="/dashboard" to="/" />
+          <Route path="/detail/:gnomeId" component={Details} />
+          <Route component={NotFound} />
+        </Switch>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
