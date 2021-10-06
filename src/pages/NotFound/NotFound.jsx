40077265cf2d7c75b7c367cc35b8bcefe5c0c739
Pau Gallardo <paugallrdocordon@gmail.com>
2021-10-06 23:25:49 +0200

    Add go back button

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NotFound() {
   return (
-    <h1>Page not Found</h1>
+    <section>
+      <h1>Page not Found</h1>
+      <Link to="/">
+        <button
+          type="button"
+        >
+          Go back
+        </button>
+      </Link>
+    </section>
   );
 }
